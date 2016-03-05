fbd51cf1671dac8f518880c81466606921788f6a
hirofumi_i <detroit69city.nau@gmail.com>
2016-03-06 02:33:52 +0900

    remove debug module

--- client.js
+++ client.js
@@ -2,35 +2,27 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import debug from 'debug';
 import app from './app';
 import Router from 'react-router';
 import FluxibleComponent from 'fluxible-addons-react/FluxibleComponent';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 
-const debugClient = debug('Example');
 const dehydratedState = window.App; // Sent from the server
 
-debugClient('rehydrating app');
-
-function RenderApp(context){
-    debugClient('React Rendering');
+function RenderApp(context) {
     const mountNode = document.getElementById('app');
 
     ReactDOM.render(
         <FluxibleComponent context={context.getComponentContext()}>
             <Router routes={context.getComponent()} history={createBrowserHistory()} />
         </FluxibleComponent>,
-        mountNode,
-        () => debugClient('React Rendered')
+        mountNode
     );
 }
 
 app.rehydrate(dehydratedState, (err, context) => {
     if (err) throw err;
 
-    window.debug = debug;
     window.context = context;
-
     RenderApp(context);
 });
--- server.js
+++ server.js
@@ -13,7 +13,6 @@ import convert from 'koa-convert';
 import path from 'path';
 import serialize from 'serialize-javascript';
 import navigateAction from './actions/navigate';
-import debugLib from 'debug';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
 import HtmlComponent from './components/Html';
@@ -21,8 +20,6 @@ import FluxibleComponent from 'fluxible-addons-react/FluxibleComponent';
 import { match, RoutingContext } from 'react-router';
 import app from './app';
 
-const env = process.env.NODE_ENV;
-const debug = debugLib('Example');
 const server = new Koa();
 
 server.use(convert(serve(path.join(__dirname, '/build'))));
@@ -30,58 +27,40 @@ server.use(convert(bodyParser()));
 server.use(convert(logger()));
 
 server.use((ctx, next) => {
-
-    const context = app.createContext();
-    debug('Executing navigate action');
-
     return new Promise((resolve, reject) => {
-
         match({routes: app.getComponent(), location: ctx.url}, (error, redirectLocation, renderProps) => {
-
             if (error) {
-
-                ctx.throw(error.message, 500);
-
+                reject(error);
             } else if (redirectLocation) {
-
                 ctx.redirect(redirectLocation.pathname + redirectLocation.search);
-
             } else if (renderProps) {
-
+                const context = app.createContext();
                 context.executeAction(navigateAction, {path: ctx.url}, () => {
-
-                    debug('Exposing context state');
                     const exposed = 'window.App=' + serialize(app.dehydrate(context)) + ';';
-
-                    debug('Rendering Application component into html');
-
                     const markup = ReactDOM.renderToString(
                         <FluxibleComponent context={context.getComponentContext()}>
                             <RoutingContext {...renderProps} />
                         </FluxibleComponent>
                     );
                     const htmlElement = React.createElement(HtmlComponent, {
-                        clientFile: env === 'production' ? 'main.min.js' : 'main.js',
+                        clientFile: process.env.NODE_ENV === 'production' ? 'main.min.js' : 'main.js',
                         context: context.getComponentContext(),
                         state: exposed,
                         markup: markup
                     });
-
                     const html = ReactDOM.renderToStaticMarkup(htmlElement);
-                    debug('Sending markup');
+
                     resolve(html);
                 });
-
             } else {
-              ctx.throw('Not Found', 404);
+              next();
             }
         });
     }).then(result => {
         ctx.response.body = '<!DOCTYPE html>' + result;
         ctx.response.type = 'html';
-        debug('Sending markup');
     }).catch(error => {
-        console.error(error);
+        ctx.throw(error.message, 500);
     });
 });
 
